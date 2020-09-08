13a1a86698b07d3e716551f49c125009c913c230
vagrant <vagrant@localhost.localdomain>
2020-09-08 04:28:55 +0000

    [Add] ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,18 +1,16 @@
 class BookCommentsController < ApplicationController
     before_action :authenticate_user!
   def create
-    book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comment = @book.book_comments.new(book_comment_params)
+    @book_comment.user_id = current_user.id
+    @book_comment.save
   end
 
   def destroy
-    comment = BookComment.find(params[:id])
-    comment.destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
   end
 
   private
